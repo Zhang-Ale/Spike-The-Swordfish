a5575a11c428731af1ffb9d40faf1ce3f866e7d7
HongChengZ <moonwolfzhang@gmail.com>
2023-10-30 15:18:05 -0400

    d

--- Game-Jam/Assets/_Scripts/PlayerMovement.cs
+++ Game-Jam/Assets/_Scripts/PlayerMovement.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Rigidbody rb;
 
-    // Update is called once per frame
-    void Update()
+    public float forwardForce = 7000f;
+    public float sidewaysForce = 120f;
+
+    public GameObject winText;
+    public GameObject lastObstacle;
+
+    void FixedUpdate()
     {
-        
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("w")) { rb.AddForce(0, 45, 0); }
+
+        if (Input.GetKey("s")) { rb.AddForce(0, -40, 0); }
+    }
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "Obstacle")
+        {
+            this.enabled = false;
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == lastObstacle)
+        {
+            winText.SetActive(true);
+        }
     }
 }
