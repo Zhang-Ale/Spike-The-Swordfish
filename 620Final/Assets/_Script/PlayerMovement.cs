24478668bdcdc207164b4ae104ad763966820deb
HongChengZ <moonwolfzhang@gmail.com>
2023-10-30 16:00:29 -0400

    d

---
+++ 620Final/Assets/_Script/PlayerMovement.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    public float forwardForce = 7000f;
+    public float sidewaysForce = 120f;
+
+    public GameObject winText;
+    public GameObject lastObstacle;
+
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+            transform.Rotate(90.0f, 0.0f, 0.0f, Space.Self);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("w")) { rb.AddForce(0, 45, 0); }
+
+        if (Input.GetKey("s")) { rb.AddForce(0, -40, 0); }
+    }
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "Obstacle")
+        {
+            this.enabled = false;
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == lastObstacle)
+        {
+            winText.SetActive(true);
+        }
+    }
+}
