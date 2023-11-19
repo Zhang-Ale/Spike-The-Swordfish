588dbe0f42b04f07a8c59c4d63e0094e1e797fb3
lindenkillam <77918412+lindenkillam@users.noreply.github.com>
2023-11-19 00:30:44 -0500

    Make randomly-spawned schools of fish

--- 620Final/Assets/_Script/FishSchoolScript.cs
+++ 620Final/Assets/_Script/FishSchoolScript.cs
@@ -5,12 +5,24 @@ using UnityEngine;
 public class FishSchoolScript : MonoBehaviour
 {
     public static List<GameObject> goalList = new List<GameObject>();
+    const int avgNumFishInSchool = 8;
     [SerializeField] int i;
     const float speed = 10f;
-    Vector3 targetDirection;
-    Vector3 newDirection;
+    Vector3 targetDirection, vector;
+    public GameObject fish;
+    GameObject newFish;
+
     void Start()
     {
+        int numFishInSchool = Random.Range(avgNumFishInSchool - 3, avgNumFishInSchool + 4);
+        for(int i = 0; i < numFishInSchool; ++i)
+        {
+            vector = new Vector3(this.transform.position.x + Random.Range(-2f, 2f),
+                this.transform.position.y + Random.Range(-1f, 1f),
+                this.transform.position.z + Random.Range(-3f, 3f));
+            newFish = Instantiate(fish, vector, Quaternion.identity);
+            newFish.transform.SetParent(this.transform);
+        }
         i = Random.Range(0, goalList.Count);
         targetDirection = goalList[i].transform.position - this.transform.position;
         this.transform.LookAt(targetDirection);
