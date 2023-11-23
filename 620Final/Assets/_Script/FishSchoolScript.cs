c89e31c94326fe16c7f93d0b0421a9236630529e
lindenkillam <77918412+lindenkillam@users.noreply.github.com>
2023-11-23 04:18:01 -0500

    Added a fleeing behavior but it's not working well

--- 620Final/Assets/_Script/FishSchoolScript.cs
+++ 620Final/Assets/_Script/FishSchoolScript.cs
@@ -5,21 +5,28 @@ using UnityEngine;
 public class FishSchoolScript : MonoBehaviour
 {
     public static List<GameObject> goalList = new List<GameObject>();
-    const int avgNumFishInSchool = 8;
+    const int avgNumFishInSchool = 8; //Average school size
+    const int schoolVariance = 3; //A school can have the average plus or minus this number of fish
+    const float xOffsetMax = 2f;
+    const float yOffsetMax = 1f;
+    const float zOffsetMax = 3f;
     [SerializeField] int i;
     const float speed = 10f;
+    const float fleeRadius = 5f;
     Vector3 targetDirection, vector;
     public GameObject fish;
+    public GameObject player;
     GameObject newFish;
 
     void Start()
     {
-        int numFishInSchool = Random.Range(avgNumFishInSchool - 3, avgNumFishInSchool + 4);
+        player = GameObject.FindWithTag("Player");
+        int numFishInSchool = Random.Range(avgNumFishInSchool - schoolVariance, avgNumFishInSchool + schoolVariance + 1);
         for(int i = 0; i < numFishInSchool; ++i)
         {
-            vector = new Vector3(this.transform.position.x + Random.Range(-2f, 2f),
-                this.transform.position.y + Random.Range(-1f, 1f),
-                this.transform.position.z + Random.Range(-3f, 3f));
+            vector = new Vector3(this.transform.position.x + Random.Range(-xOffsetMax, xOffsetMax),
+                this.transform.position.y + Random.Range(-yOffsetMax, yOffsetMax),
+                this.transform.position.z + Random.Range(-zOffsetMax, zOffsetMax));
             newFish = Instantiate(fish, vector, Quaternion.identity);
             newFish.transform.SetParent(this.transform);
         }
@@ -30,13 +37,23 @@ public class FishSchoolScript : MonoBehaviour
 
     void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, goalList[i].transform.position,
-             speed * Time.deltaTime);
-
-        if(Vector3.Distance(goalList[i].transform.position, this.transform.position) < 1)
+        if(Vector3.Distance(this.transform.position, player.transform.position) < fleeRadius) //Run away from player
+        {
+            Vector3 fleeDirection = (this.transform.position - player.transform.position).normalized;
+            this.transform.position = Vector3.MoveTowards(this.transform.position, fleeDirection * fleeRadius,
+                speed * Time.deltaTime);
+            this.transform.LookAt(fleeDirection);
+        }
+        else if(Vector3.Distance(goalList[i].transform.position, this.transform.position) < 1) //Switch goals if close to a goal
         {
             i = Random.Range(0, goalList.Count);
-            targetDirection = goalList[i].transform.position - this.transform.position;
+            targetDirection = (goalList[i].transform.position - this.transform.position).normalized;
+            this.transform.LookAt(targetDirection);
+        }
+        else
+        {
+            this.transform.position = Vector3.MoveTowards(this.transform.position, goalList[i].transform.position,
+                speed * Time.deltaTime);
             this.transform.LookAt(targetDirection);
         }
     }
